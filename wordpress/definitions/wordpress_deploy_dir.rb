8ca11a39822fdd8bbad7f55ab9207987f8d1b119
Drew J. Sonne <drew.sonne@gmail.com>
2013-09-01 11:25:19 +1000

    Fixed incorrect variable.

--- wordpress/definitions/wordpress_deploy_dir.rb
+++ wordpress/definitions/wordpress_deploy_dir.rb
@@ -1,7 +1,7 @@
 define :wordpress_deploy_dir do
 
   bash "chmod wp-content" do
-    user deploy[:user]
+    user params[:user]
     cwd params[:path]
     code "chmod -R g+rwX #{params[:path]}"
   end
