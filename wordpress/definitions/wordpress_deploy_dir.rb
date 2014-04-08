b126c9d6f57e35c5c93b148ad2c87d024e7f9e41
Drew J. Sonne <drew.sonne@gmail.com>
2014-04-08 11:58:17 +1000

    Added check for directory before chmod.

--- wordpress/definitions/wordpress_deploy_dir.rb
+++ wordpress/definitions/wordpress_deploy_dir.rb
@@ -1,9 +1,11 @@
 define :wordpress_deploy_dir do
 
-  bash "chmod wp-content" do
-    user params[:user]
-    cwd params[:path]
-    code "chmod -R g+rwX #{params[:path]}"
+  if File.directory?(params[:path])
+    bash "chmod wp-content" do
+      user params[:user]
+      cwd params[:path]
+      code "chmod -R g+rwX #{params[:path]}"
+    end
   end
 
 end
